e59efdc4fa93ca4f4d2418e2aaa54544c08b21e6
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-01-28 12:24:27 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles/GlobalStyles';
 import * as theme from './styles/themes';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme['defaultTheme']}>
+      <GlobalStyles />
       <p>Olá, mundo!</p>
     </ThemeProvider>
   );
