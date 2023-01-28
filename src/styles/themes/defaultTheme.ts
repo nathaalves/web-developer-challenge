80b3180c3ce4dcd3eccf8779a8d3dd8b61bd08a8
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-01-28 12:17:55 -0300

    feat: create default theme

---
+++ src/styles/themes/defaultTheme.ts
@@ -0,0 +1,34 @@
+export const defaultTheme = {
+  title: 'default',
+  color: {
+    green: {
+      '500': '#71bb00',
+    },
+    grey: {
+      '200': '#9F9F9F',
+      '300': '#7A7A7A',
+      '400': '#5F5F5F',
+      '500': '#494949',
+      '600': '#3B3B3B',
+      '700': '#343434',
+      '800': '#313131',
+      '900': '#2B2B2B',
+    },
+  },
+  fontFamily: {
+    base: 'Roboto, sans-serif',
+  },
+  fontSize: {
+    sm: '0.75rem',
+    base: '0.875rem',
+    lg: '1rem',
+  },
+  space: {
+    xs: '8px',
+    sm: '12px',
+    base: '16px',
+    lg: '24px',
+    xl: '32px',
+    '2xl': '56px',
+  },
+};
